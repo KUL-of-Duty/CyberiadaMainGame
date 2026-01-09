85d2a974fbf738503077449c2a2dac58f90232a6
Kain <Kainnoniero@gmail.com>
2026-01-09 18:44:40 +0100

    usunięcie co sie da
    my bad

--- Assets/Scripts/ItemData.cs
+++
@@ -1,9 +0,0 @@
-// ItemData.cs
-using UnityEngine;
-
-public class ItemData : MonoBehaviour
-{
-    // TO POLE MUSI BYĆ PUBLICZNE (lub [SerializeField]), aby pojawiło się w Inspektorze.
-    // Musisz mieć zdefiniowany ItemType (enum z pliku ItemType.cs)
-    [SerializeField] public ItemType itemType; 
-}
