814b5463a17c24a5331635963c4c32fb4acc9c7a
Kain <Kainnoniero@gmail.com>
2026-03-06 17:26:31 +0100

    DZIAŁAJĄCYMANAGER
    chodzenie sprint i skok

--- Assets/Scripts/Player/ClientMovement.cs
+++ Assets/Scripts/Player/ClientMovement.cs
@@ -3,59 +3,46 @@ using UnityEngine;
 
 public class ClientMovement : NetworkBehaviour
 {
-    [Header("Player Controllers")]
-    [SerializeField]
-    CharacterController _characterController;
-    [SerializeField]
-    PlayerMovement _playerMovement;
+    [SerializeField] CharacterController _characterController;
+    [SerializeField] PlayerMovement _playerMovement;
+    [SerializeField] PlayerSoundManager _soundManager;
 
     private void Awake()
     {
+        if (_playerMovement == null) _playerMovement = GetComponent<PlayerMovement>();
+        if (_soundManager == null) _soundManager = GetComponent<PlayerSoundManager>();
         _playerMovement.enabled = false;
     }
 
     public override void OnNetworkSpawn()
     {
-        base.OnNetworkSpawn();
-
         if (IsOwner)
         {
             _playerMovement.enabled = true;
-            _characterController.enabled = true;
         }
-
-        if (IsServer)
-        {
-            _characterController.enabled = true;
-        }
-
+        if (IsServer) _characterController.enabled = true;
     }
 
     [ServerRpc]
-    public void UpdateMovementServerRPC(Vector3 move)
-    {
-        _characterController.Move(move);    
-    }
+    public void UpdateMovementServerRPC(Vector3 move) => _characterController.Move(move);
 
     [ServerRpc]
-    public void UpdateRotationServerRPC(Vector3 rotation)
-    {
-        transform.Rotate(rotation);
-    }
+    public void UpdateRotationServerRPC(Vector3 rotation) => transform.Rotate(rotation);
 
     [ServerRpc]
-    public void UpdateGravitationServerRPC(Vector3 gravitationalPull)
-    {
-        _characterController.Move(gravitationalPull);
-    }
+    public void UpdateGravitationServerRPC(Vector3 gravitationalPull) => _characterController.Move(gravitationalPull);
+
+    [ServerRpc]
+    public void RequestJumpSoundServerRPC() => _soundManager.PlayJumpSoundClientRPC();
+
+    [ServerRpc]
+    public void RequestStepSoundServerRPC(bool isSprinting) => _soundManager.PlayStepSoundClientRpc(isSprinting);
 
     [ServerRpc]
     public void TeleportPlayerServerRPC(Vector3 location)
     {
         _characterController.enabled = false;
-
         transform.position = location;
-
         _characterController.enabled = true;
     }
 }
